e55b4db771478cf15ebdc92ed9966fcdf171b1fe
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-12-24 00:33:00 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,14 @@
 def pascal_triangle(n):
-      if n <= 0:
-            return []
-      res = [[1]]
+    if n <= 0:
+        return []
+    res = [[1]]
         
-      for i in range(n - 1):
-          temp = [0] + res[-1] + [0]
-          row = []
-          for j in range(len(res[-1])+1):
-              row.append(temp[j] + temp[j+1])
-          res.append(row)
-      
-      return res
-      
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1])+1):
+            row.append(temp[j] + temp[j+1])
+        res.append(row)
     
+    return res
+
