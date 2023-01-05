95e06496f69ba0bffc6346d807ebd70ba39bf9cb
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-05 22:18:47 +0300

    Create 0-lockboxes.py

---
+++ 0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin python3
+
+from typing import *
+
+def canUnlockAll(boxes: List) -> bool:
+    """
+    accepts list of boxes(lists) containing
+    keys to other boxes. 
+    The first box is always opened.
+    
+    Return True if all boxes can be opened else False
+    """
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
